f49452e01021b1a1bd6393b6126a4be21659b3b2
arjun <arjunporwal10@gmail.com>
2018-07-09 16:22:27 +0530

    linting and test cases passed

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,12 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+function revers(str) {
+  return str.split('').reverse().join('');
+}
+const reverse = (text) => {
+  const str = text;
+  const rever = revers(str);
+  return rever;
+};
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,11 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (text) => {
+  const arrlen = text.length;
+  let newstr = '';
+  for (let i = arrlen - 1; i >= 0; i -= 1) {
+    newstr += text[i];
+  }
+  return newstr;
+};
 
 module.exports = reverse;
